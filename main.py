5aecab1be9cf89312bd1c7c499861a6f17d321dc
Matthew Nguyen <m.nguyen032204@gmail.com>
2026-04-05 19:21:02 -0500

    Starting room.html development
    Why: Need initial HTML and CSS layout for room.html and back-end route to room.html for testing
    
    How: Implemented flex and grid HTML layout styled with CSS and added route in back-end to enter room.html

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
@@ -7,7 +7,22 @@ socketio = SocketIO(app)
 
 @app.route("/", methods=["POST", "GET"])
 def home():
+    # A little bit done early to route to room.html for testing front-end
+    # feel free to change but leave some way to enter room.html ty -matthew
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+
+        if not name or not code:
+            return render_template("home.html")
+        else:
+            return redirect(url_for("room"))
+        
     return render_template("home.html")
+
+@app.route("/room")
+def room():
+    return render_template("room.html")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
